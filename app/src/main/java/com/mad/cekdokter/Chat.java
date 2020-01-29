72d84d4704796f8c0cee768a6f8fb898568abd3d
tankelvin <38410907+tankelvin@users.noreply.github.com>
2020-01-30 05:29:41 +0700

    Update Chat.java

--- app/src/main/java/com/mad/cekdokter/Chat.java
+++ app/src/main/java/com/mad/cekdokter/Chat.java
@@ -3,10 +3,21 @@ package com.mad.cekdokter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+
 public class Chat extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+    private EditText edtEmail;
+    private EditText edtPass;
+    private Button btnMasuk;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
